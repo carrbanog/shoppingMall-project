a50694b7a16dd2e9cfba9f5dc61451962de4b31b
carrbanog <loopang5954@gmail.com>
2024-10-14 12:04:16 +0900

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+
+          {/* <Route path="/" element={<Shop/>} */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from "react";
+import "./Navbar.css";
+import all_product from "../Assets/all_product";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaOpencart } from "react-icons/fa6";
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("shop");
   return (
-    <div className='navbar'>
-
+    <div className="navbar">
+      <div className="nav-logo">
+        <FaOpencart />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => {setMenu("shop")}}>Shop{menu === "shop" ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu("t-shirt")}}>T-shirt{menu === "t-shirt" ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu("cap")}}>Cap{menu === "cap" ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu("pants")}}>Pants{menu === "pants" ? <hr/> : <></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <FaShoppingCart />
+        <div className="nav-cart-count">
+          0
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
